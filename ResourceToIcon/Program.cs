36b553a25ec5f41e561898e97e5f4fab4f2d5583
Oleg Filippov <35500737+folegv75@users.noreply.github.com>
2018-01-22 08:59:53 +0300

    1

--- ResourceToIcon/Program.cs
+++ ResourceToIcon/Program.cs
@@ -1,17 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ResourceToIcon
 {
+    public class IconResource
+    {
+        List<string> TextBytes;
+
+        public IconResource()
+        {
+            TextBytes = new List<string>();
+        }
+
+        public void AppendTextBytes(string ByteStr)
+        {
+            var s = ByteStr.Replace('\'',' ');
+            TextBytes.Add(s.Trim());
+        }
+    }
+
+    class Worker
+    {
+        public string Filename { get; set; }
+
+        List<IconResource> Icons = new List<IconResource>();
+
+        public void Work()
+        {
+            if (!File.Exists(Filename))
+            {
+                Console.WriteLine($"Файл не найден:{Filename}");
+                return;
+            }
+
+            using (StreamReader sr = new StreamReader(Filename))
+            {
+
+                string line;
+                bool IsIconStart = false;
+                IconResource currIcon = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (IsIconStart)
+                    {
+                        if (line == "}")
+                        {
+                            Icons.Add(currIcon);
+                        } else
+                        {
+                            currIcon.AppendTextBytes(line);
+                        }
+
+                    }
+                    else
+                    {
+                        if (line == "{")
+                        {
+                            IsIconStart = true;
+                            currIcon = new IconResource();
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет");
-            Console.ReadLine();
+            if (args.Count()!=1)
+            {
+                Console.WriteLine("Укажите файл в параметрах типа rc, содержащий текстовое описание иконок (Borland)");
+                return;
+            }
+            var W = new Worker
+            {
+                Filename = args[0]
+            };
+            W.Work();
         }
     }
 }
