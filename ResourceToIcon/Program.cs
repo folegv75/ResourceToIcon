8d9ff82794512fef71fe674bd5dc8026b62187a5
Oleg Filippov <35500737+folegv75@users.noreply.github.com>
2018-01-23 08:26:49 +0300

    Result

--- ResourceToIcon/Program.cs
+++ ResourceToIcon/Program.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ResourceToIcon
+namespace ConsoleApp1
 {
     public class IconResource
     {
+        public const int IconLength = 16 * 48 - 2;
+
         List<string> TextBytes;
+        public byte[] Bin;
 
         public IconResource()
         {
@@ -18,23 +21,40 @@ namespace ResourceToIcon
 
         public void AppendTextBytes(string ByteStr)
         {
-            var s = ByteStr.Replace('\'',' ');
+            var s = ByteStr.Replace('\'', ' ');
             TextBytes.Add(s.Trim());
+        }
+
+        public void ConvertToBin()
+        {
+            Bin = new byte[IconLength];
+            int BinIndex = 0;
+            foreach (var someStr in TextBytes)
+            {
+                var HexChars = someStr.Split(' ');
+                for (int i = 0; i < HexChars.Length; i++)
+                {
+
+                    Bin[BinIndex] = Convert.ToByte(HexChars[i], 16);
+                    BinIndex++;
+                }
+            }
         }
     }
 
     class Worker
     {
         public string Filename { get; set; }
+        public string OutputDir;
 
         List<IconResource> Icons = new List<IconResource>();
 
-        public void Work()
+        bool ReadTextIcon()
         {
             if (!File.Exists(Filename))
             {
                 Console.WriteLine($"Файл не найден:{Filename}");
-                return;
+                return false;
             }
 
             using (StreamReader sr = new StreamReader(Filename))
@@ -49,8 +69,10 @@ namespace ResourceToIcon
                     {
                         if (line == "}")
                         {
+                            IsIconStart = false;
                             Icons.Add(currIcon);
-                        } else
+                        }
+                        else
                         {
                             currIcon.AppendTextBytes(line);
                         }
@@ -66,17 +88,42 @@ namespace ResourceToIcon
                     }
                 }
             }
+            return true;
+        }
+
+        void ConvertTextToByte()
+        {
+            foreach (var someIcon in Icons) someIcon.ConvertToBin();
+        }
+
+        void SaveToIconFile()
+        {
+            int IconCount = 1;
+            foreach (var someIcon in Icons)
+            {
+                string fileName = OutputDir + "\\Icon" + string.Format("{0:d3}", IconCount) + ".ico";
+                using (BinaryWriter bw = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+                {
+                    bw.Write(someIcon.Bin, 0, IconResource.IconLength);
+                }
+                IconCount++;
+            }
+        }
+
+        public void Work()
+        {
+            if (!ReadTextIcon()) return;
+            ConvertTextToByte();
+            SaveToIconFile();
 
         }
     }
 
     class Program
     {
-        
-
         static void Main(string[] args)
         {
-            if (args.Count()!=1)
+            if (args.Count() != 1)
             {
                 Console.WriteLine("Укажите файл в параметрах типа rc, содержащий текстовое описание иконок (Borland)");
                 return;
@@ -85,6 +132,9 @@ namespace ResourceToIcon
             {
                 Filename = args[0]
             };
+            var f = new FileInfo(W.Filename);
+
+            W.OutputDir = f.DirectoryName;
             W.Work();
         }
     }
